08f4da347555122d06ceb25ab4c2ec4af4ae3fce
Bohdan M. <re.proxy0@gmail.com>
2026-06-18 18:15:25 +0300

    Add command to count unique IPs in access.log

--- tasks.sh
+++ tasks.sh
@@ -3,3 +3,6 @@ cut -d " " -f1 access.log | sort | uniq -c |  sort -nr | head -n 5
 
 # find all records with 404 codes and write to the file
 grep 404 access.log > 404s.txt
+
+# count unique IP's
+cut -d " " -f1 access.log | sort | uniq | wc -l
