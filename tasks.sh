2b9a4ae90ea2913cf0c29e97bfb62913d7a2833e
Bohdan M. <re.proxy0@gmail.com>
2026-06-18 18:11:19 +0300

    Add grep command to extract 404 error records
    Added functionality to find and save 404 error records.

--- tasks.sh
+++ tasks.sh
@@ -1,3 +1,5 @@
 # show the top 5 IP addresses by number of requests + number of requests
-
 cut -d " " -f1 access.log | sort | uniq -c |  sort -nr | head -n 5
+
+# find all records with 404 codes and write to the file
+grep 404 access.log > 404s.txt
